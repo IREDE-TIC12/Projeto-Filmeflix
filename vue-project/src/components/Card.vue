495a5d0c3a312ed2877d296a9939440bd26a1db3
VictoriaRSsousa <victoriars.sousa@gmail.com>
2024-06-04 11:00:31 -0300

    feat: create component

---
+++ vue-project/src/components/Card.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Card',
+})
+</script>
+
+<template>
+    <div class="h-[610px] w-[20%] bg-black rounded-xl">
+        <p class="text-white flex justify-center "> FILME </p>
+    </div>
+</template>
---
+++ vue-project/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavBar',
+})
+</script>
+
+<template>
+    <nav class="bg-gray-700">
+    <p class="text-white">HOME</p>
+    </nav>
+</template>
